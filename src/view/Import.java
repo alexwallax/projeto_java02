77ae07551e8cbc6dff0886a5a2e6e3ac2ca482ba
Alex Wallace <alexwallax@gmail.com>
2022-03-14 16:54:55 -0300

    Import

---
+++ src/view/Import.java
@@ -0,0 +1,20 @@
+package view;
+
+import java.util.Date;
+
+import javax.swing.JButton;
+
+public class Import {
+	
+	public static void main(String[] args) {
+		java.lang.String b = "Boa tarde!";
+		java.lang.System.out.println(b);
+		
+		Date d = new Date();
+		System.out.println(d);
+		
+		JButton botao = new JButton();
+		
+	}
+
+}
