b1a957764129bc394414a7279064dd13423c8a20
Alex Wallace <alexwallax@gmail.com>
2022-03-16 14:40:30 -0300

    conversão de tipos

---
+++ src/view/ConversaoNum.java
@@ -0,0 +1,24 @@
+package view;
+
+public class ConversaoNum {
+	
+	public static void main(String[] args) {
+		
+		double a = 1; // conversão implícita
+		System.out.println(a);
+		
+		float b = (float) 1.12345; // conversão explícita (CAST)
+		System.out.println(b);
+		
+		int c = 127;
+		byte d = (byte) c; // conversão explícita (CAST)
+		System.out.println(d);
+		
+		double e = 1.999999;
+		int f = (int) e; // conversão explícita (CAST)
+		System.out.println(f);
+		
+		
+	}
+
+}
